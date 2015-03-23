4422ef8bca21ba121f955d58943932877a7199eb
Jon Portnoy <avenj@cobaltirc.org>
2015-03-22 20:08:47 -0400

    refactor to use Math::Calc::Parser

--- lib/Bot/Cobalt/Plugin/Calc.pm
+++ lib/Bot/Cobalt/Plugin/Calc.pm
@@ -1,58 +1,42 @@
 package Bot::Cobalt::Plugin::Calc;
-our $VERSION = '0.003_01';
 
-use 5.12.1;
-use strictures 1;
+use strictures 2;
 
 use Bot::Cobalt;
 use Bot::Cobalt::Common;
 
-use Bot::Cobalt::Plugin::Calc::Parser;
+use Math::Calc::Parser ();
 
-sub new { bless {}, shift }
+sub new { bless [], shift }
 
 sub Cobalt_register {
   my ($self, $core) = splice @_, 0, 2;
-  
-  register( $self, 'SERVER',
-
-    'public_cmd_calc',
-
-  );
-  
-  logger->info("Loaded ($VERSION)");
-  
-  return PLUGIN_EAT_NONE
+  register( $self, SERVER => 'public_cmd_calc' );
+  logger->info("Loaded: calc");
+  PLUGIN_EAT_NONE
 }
 
 sub Cobalt_unregister {
   my ($self, $core) = splice @_, 0, 2;
-  
-  $core->unloader_cleanup('Bot::Cobalt::Plugin::Calc::Parser');
-  
   logger->info("Unloaded");  
-  
-  return PLUGIN_EAT_NONE
+  PLUGIN_EAT_NONE
 }
 
 sub Bot_public_cmd_calc {
   my ($self, $core) = splice @_, 0, 2;
   my $msg     = ${ $_[0] };
   
-  my $calc = Bot::Cobalt::Plugin::Calc::Parser->new;
-  
   my $msgarr  = $msg->message_array;
-  my $calcstr = join '', @$msgarr;
-  
-  my $result;
-  eval { $result = $calc->from_string($calcstr) };
-  $result = "Parser said: $@" if $@;
+  my $calcstr = join ' ', @$msgarr;
+  my $result  = Math::Calc::Parser->try_evaluate($calcstr);
 
-  broadcast( 'message', $msg->context, $msg->channel,
-    $msg->src_nick.": $result"
+  my $reply = defined $result ? $result : "err: ".Math::Calc::Parser->error;
+
+  broadcast( message => $msg->context, $msg->channel,
+    $msg->src_nick . ": $reply"
   );
   
-  return PLUGIN_EAT_ALL
+  PLUGIN_EAT_NONE
 }
 
 
@@ -63,31 +47,23 @@ __END__
 
 =head1 NAME
 
-Bot::Cobalt::Plugin::Calc - Simple calculator for Bot::Cobalt
+Bot::Cobalt::Plugin::Calc - Calculator plugin for Bot::Cobalt
 
 =head1 SYNOPSIS
 
-  !calc (2+2)*(2+4)
-  !calc 0xff
-  !calc 0644
+  # See Math::Calc::Parser ->
+  !calc 2 + 2
+  !calc 0xff << 2
+  !calc int rand 5
 
 =head1 DESCRIPTION
 
-Simple L<Bot::Cobalt> calculator plugin using L<Parser::MGC>.
+A L<Bot::Cobalt> calculator plugin using L<Math::Calc::Parser>.
 
-Understands - + * / ^ operations.
-
-Also understands hex and octal.
-
-=head1 TODO
-
-A RPN-style calculator with a persistent per-user stack is planned as 
-sort of an IRC C<dc> ... eventually.
+See the L<Math::Calc::Parser> documentation for details.
 
 =head1 AUTHOR
 
 Jon Portnoy <avenj@cobaltirc.org>
-
-L<http://www.cobaltirc.org>
 
 =cut
--- t/01_loadable.t
+++ t/01_loadable.t
@@ -1,12 +1,10 @@
-use Test::More tests => 6;
+use Test::More;
+use strict; use warnings;
 
 BEGIN {
-  use_ok( 'Bot::Cobalt::Plugin::Calc::Parser' );
   use_ok( 'Bot::Cobalt::Plugin::Calc' );
 }
-
-new_ok( 'Bot::Cobalt::Plugin::Calc::Parser' );
-can_ok( 'Bot::Cobalt::Plugin::Calc::Parser', 'from_string' );
-
 new_ok( 'Bot::Cobalt::Plugin::Calc' );
 can_ok( 'Bot::Cobalt::Plugin::Calc', 'Cobalt_register', 'Cobalt_unregister' );
+
+done_testing
